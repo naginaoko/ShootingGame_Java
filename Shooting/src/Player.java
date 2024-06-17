360ca80c1b5f99932d4cee4a8f1755750796be7c
naginaoko <zeal23410186@fsgcl.jp>
2024-06-17 12:15:17 +0900

    練習問題10-2

--- Shooting/src/Player.java
+++ Shooting/src/Player.java
@@ -27,7 +27,7 @@ public class Player extends Character implements KeyListener{
 			vx=0;
 		}
 		if(e.getKeyCode()==KeyEvent.VK_SPACE) {
-			GameWorld.playerBullets.add(new PlayerBullet(x,y,0,-10));
+			GameWorld.playerBullets.add(new PlayerBullet(x,y,5,-10));
 			System.out.println("弾の数="+GameWorld.playerBullets.size());
 		}
 	}
