c9e5d255e555ecc15e2736af8d2703ab0cf7881b
naginaoko <zeal23410186@fsgcl.jp>
2024-06-24 10:58:45 +0900

    Java12.3章のコミット

--- Shooting/src/GameFrame.java
+++ Shooting/src/GameFrame.java
@@ -43,8 +43,7 @@ public class GameFrame extends MyFrame{
 	public void checkPlayerAndEnemies() {
 		for(int i=0; i<GameWorld.enemies.size(); i++) {
 			Enemy e=GameWorld.enemies.get(i);
-			if(Math.abs(e.x-GameWorld.player.x)<=30 && 
-			   Math.abs(e.y-GameWorld.player.y)<=30) {
+			if(checkHit(GameWorld.player,e)) {
 				System.out.println("やられた!");
 				GameWorld.player.y=-1000;
 			}
@@ -59,8 +58,7 @@ public class GameFrame extends MyFrame{
 			int j=0;
 			while(j<GameWorld.enemies.size()) {
 				Enemy e=GameWorld.enemies.get(j);
-				if(Math.abs(e.x-b.x)<=30 &&
-				   Math.abs(e.y-b.y)<=30) {
+				if(checkHit(e,b)) {
 					System.out.println("あたり");
 					hits++;
 					GameWorld.enemies.remove(j);
@@ -74,6 +72,10 @@ public class GameFrame extends MyFrame{
 				i++;
 			}
 		}
+	}
+	
+	public boolean checkHit(Character a,Character b) {
+		return Math.abs(a.x-b.x)<=30 && Math.abs(a.y-b.y)<=30;
 	}
 
 }
